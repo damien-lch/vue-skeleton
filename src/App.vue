695422c45e8ea559b852f693370f4c78d684cc7d
Damien <hello@neiwad.io>
2021-03-08 21:53:39 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section>
+      <h1>Skeletons</h1>
+      <div class="items">
+        <div class="item">
+          <Skeleton width="50%" height="80%" borderRadius="40" />
+        </div>
+        <div class="item">
+          <Skeleton width="100" height="200" />
+        </div>
+        <div class="item">
+          <Skeleton width="200" height="200" rounded />
+        </div>
+        <div class="item"></div>
+        <div class="item"></div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.items {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  .item {
+    border: 2px solid #f5f5f5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 }
 </style>
---
+++ src/components/Skeleton.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="skeleton" :style="styles"></div>
+</template>
+
+<script>
+export default {
+  name: "skeleton",
+  props: {
+    width: {
+      type: String,
+      default: "100",
+    },
+    height: {
+      type: String,
+      default: "100",
+    },
+    color: {
+      type: String,
+      default: "#E0E0E0",
+    },
+    rounded: {
+      type: Boolean,
+      default: false,
+    },
+    borderRadius: {
+      type: String,
+      default: "8",
+    },
+  },
+  computed: {
+    styles() {
+      const styles = {
+        width: this.width.includes("%") ? this.width : this.width + "px",
+        height: this.height.includes("%") ? this.height : this.height + "px",
+        backgroundColor: this.color,
+        borderRadius: this.rounded ? "50%" : this.borderRadius + "px",
+      };
+      return styles;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.skeleton {
+  position: relative;
+  overflow: hidden;
+  &::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    transform: translateX(-100%);
+    background-image: linear-gradient(
+      90deg,
+      rgba(#fff, 0) 0,
+      rgba(#fff, 0.2) 20%,
+      rgba(#fff, 0.5) 60%,
+      rgba(#fff, 0)
+    );
+    animation: shimmer 2s infinite;
+    content: "";
+  }
+
+  @keyframes shimmer {
+    100% {
+      transform: translateX(100%);
+    }
+  }
+}
+</style>
