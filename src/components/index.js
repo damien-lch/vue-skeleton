12ed6772d2eee4fae4fd0777f2380d022bdbe6c5
Damien <hello@neiwad.io>
2021-03-09 19:24:48 -0500

    try fix for vue3

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
-import Vue from "vue";
+//import Vue from "vue";
 import Skeleton from "./Skeleton.vue";
 
 const Components = {
   Skeleton
 };
 
+/*
 Object.keys(Components).forEach(name => {
   Vue.component(name, Components[name]);
 });
+*/
 
 export default Components;
